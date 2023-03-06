facaa19e1773dca53981ba2d8eea414ee6245b75
vitalie-butnaru <50948515+vitalie-butnaru@users.noreply.github.com>
2023-03-06 20:54:48 +0200

    TRAD

--- bot.py
+++ bot.py
@@ -6,19 +6,20 @@ Token =os.environ.get("BOT_TOKEN",None)
 updater = Updater( Token ,use_context = True )
 
 def start(updater,context):
- updater.message.reply_text('''Hi iam welcome messanger bot 
-Add me to your group 
+ updater.message.reply_text('''
  
- Made with Love ❤️ by @TechMagazineYT
+ 😇Mă bucur pentru înregistrarea ta la MasterClass! 
+ 
+Apreciez acest pas asumat! 💫
 
-  ''')
+''')
 def help(updater,context):
- updater.message.reply_text("Add me to your group ")
+ updater.message.reply_text("Adaugă-mă în canal ")
  
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to TM support Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Bun Venit la MEDCLUB Vă mulțumim că v-ați alăturat  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
